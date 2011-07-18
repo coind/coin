85af509ec10400f345733a1da67be835e51c3da5
unknown <820136687@qq.com>
2011-07-18 17:35:07 +0800

    initialize project by coin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookshopOnline::Application.config.secret_token = 'f0a963af0e16becfe2c9981a8527560bb34267834122b15eb0a89ef447098f3af762ebbaaf32165994b78887494aaf0ebab5ce1374edfadf66cd09c18b8f9b24'
